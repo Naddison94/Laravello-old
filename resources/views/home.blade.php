a52c59a4a1a494b5abc4ef9b23a316427bfa4cbc
Naddison94 <nathan@skillsforward.co.uk>
2021-05-29 22:46:44 +0100

    Playing around

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layout')
 
 @section('header')
-    <h1>Home</h1>
+    <h1 class="center">
+        Home
+    </h1>
 @endsection
 
 @section('nav')
@@ -9,13 +11,16 @@
 @endsection
 
 @section('body')
+    <h2 class="center">
+        Hello there, this is my site
+    </h2>
 
-    <h2>Hello there, this is my site</h2>
-    <p>I plan to create some functionality that will allow users to add/edit/delete(archive) posts and upload images to these posts <br>
+    <p class="center">
+        I plan to create some functionality that will allow users to add/edit/delete(archive) posts and upload images to these posts <br>
        After this is done I will decide the next steps I want to take
     </p>
 @endsection
 
 @section('footer')
-<img src="/img/spooderman.jpg">
+<img class="center" src="/img/spooderman.jpg">
 @endsection
